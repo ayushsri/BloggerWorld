d37d0af15ee59e512e68129f048535139fc8f08b
Ayush-Srivastava-northeastern <srivastava.ayu@northeastern.edu>
2021-04-08 22:17:27 -0400

    Adding bootstrap and pushing some changes with boot strap

--- frontend/src/Components/Hompage/HomePage.js
+++ frontend/src/Components/Hompage/HomePage.js
@@ -3,8 +3,11 @@ import React from "react";
 
 const Home = () => (
     <div>
-        <div>
-            <h2>Blogger World</h2>
+        <div className="jumbotron jumbotron-fluid bg-dark text-white">
+            <h4 className="display-3">Blogger World</h4>
+        </div>
+        <div className="container">
+
         </div>
     </div>
 );
