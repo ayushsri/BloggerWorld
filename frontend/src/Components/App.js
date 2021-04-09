7d4d9538322a46feda1f96e1a36d086a561ca587
Ayush-Srivastava-northeastern <srivastava.ayu@northeastern.edu>
2021-04-09 01:16:04 -0400

    making changes in navbar

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,10 +1,12 @@
 import logo from '../logo.svg';
 import './App.css';
 import HomePage from "./Hompage/HomePage"
+import NavBar from "./Hompage/NavBar";
 
 function App() {
   return (
     <div className="App">
+        <NavBar/>
      <HomePage/>
     </div>
   );
