31462b5353b2fd116c895e7cf4fb3b3851329b84
Ayush-Srivastava-northeastern <srivastava.ayu@northeastern.edu>
2021-04-15 17:36:50 -0400

    adding Blog New

---
+++ frontend/src/Components/Blogs/BlogNew.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { userCreate } from "./BlogsAPI";
+import { isAuthenticated } from "../auth";
+
+
+class BlogNew extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: {},
+            fileSize: 0,
+            loading: false,
+            redirectToProfile: false,
+            title: "",
+            body: "",
+            photo: "",
+            error: ""
+        };
+    }
+    blogNewForm = (title, body) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Post Photo</label>
+                <input
+                    onChange={this.onChange("photo")}
+                    type="file"
+                    accept="image/*"
+                    className="form-control"
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Title</label>
+                <input
+                    onChange={this.onChange("title")}
+                    type="text"
+                    className="form-control"
+                    value={title}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Body</label>
+                <textarea
+                    onChange={this.onChange("body")}
+                    type="text"
+                    className="form-control"
+                    value={body}
+                />
+            </div>
+
+            <button
+                onClick={this.onSubClick}
+                className="btn btn-raised btn-info"
+            >
+                Create Post
+            </button>
+        </form>
+    );
+    render() {
+        const {
+            title,
+            body,
+            photo,
+            user,
+            error,
+            loading,
+            redirectToProfile
+        } = this.state;
+
+        if (redirectToProfile) {
+            return <Redirect to={`/user/${user._id}`} />;
+        }
+
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Create a new post</h2>
+                <div
+                    className="alert alert-danger"
+                    style={{ display: error ? "" : "none" }}
+                >
+                    {error}
+                </div>
+
+                {loading ? (
+                    <div className="jumbotron text-center">
+                        <h2>Loading...</h2>
+                    </div>
+                ) : (
+                    ""
+                )}
+
+                {this.blogNewForm(title, body)}
+            </div>
+        );
+    }
+
+    onChange = name => event => {
+        this.setState({ error: "" });
+        const value =
+            name === "photo" ? event.target.files[0] : event.target.value;
+
+        const fileSize = name === "photo" ? event.target.files[0].size : 0;
+        this.postData.set(name, value);
+        this.setState({ [name]: value, fileSize });
+    };
+
+
+    onSubClick = event => {
+        event.preventDefault();
+        this.setState({ loading: true });
+
+        if (this.isValid()) {
+            const userId = isAuthenticated().user._id;
+            const token = isAuthenticated().token;
+
+            userCreate(userId, token, this.postData).then(data => {
+                if (data.error) this.setState({ error: data.error });
+                else {
+                    this.setState({
+                        loading: false,
+                        title: "",
+                        body: "",
+                        redirectToProfile: true
+                    });
+                }
+            });
+        }
+    };
+
+    isValid = () => {
+        const { title, body, fileSize } = this.state;
+        if (fileSize > 100000) {
+            this.setState({
+                error: "File size should be less than 100kb",
+                loading: false
+            });
+            return false;
+        }
+        if (title.length === 0 || body.length === 0) {
+            this.setState({ error: "All fields are required", loading: false });
+            return false;
+        }
+        return true;
+    };
+
+
+
+    componentDidMount() {
+        this.postData = new FormData();
+        this.setState({ user: isAuthenticated().user });
+    }
+}
+
+export default BlogNew;
+
