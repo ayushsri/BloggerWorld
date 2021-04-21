6a4cfab867a3ef16d4e1e7a9ee3a5601b93011db
Ayush-Srivastava-northeastern <srivastava.ayu@northeastern.edu>
2021-04-21 15:12:05 -0400

    few changes

--- webapp/src/Components/Hompage/HomePage.js
+++ webapp/src/Components/Hompage/HomePage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Blogs from "../Blogs/Blogs";
-
+//the home page which will include navbara and body
 const Homepage = () => (
     <div>
+    //body below navbar
         <div className="jumbotron bg-dark text-white">
             <h1 className="lead">Blogger World</h1><br></br>
             <p className="font-weight-light">"Blog your heart out." BloggerWorld allows you to to express your views and opinion on whats so
                 ever topic you want.</p>
         </div>
+    //all the blog section
         <div className="container-fluid">
             <Blogs/>
         </div>
