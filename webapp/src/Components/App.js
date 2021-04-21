ab0fcc50768ee5a38fbcf308a25a970c7a939024
Ayush-Srivastava-northeastern <77870719+Ayush-Srivastava-northeastern@users.noreply.github.com>
2021-04-21 14:18:34 -0400

    Adding a comment

--- webapp/src/Components/App.js
+++ webapp/src/Components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ProgrammeRouter from "./ProgrammeRouter";
-
+//To display using Programme router
 const App = () => (
     <BrowserRouter>
         <ProgrammeRouter />
