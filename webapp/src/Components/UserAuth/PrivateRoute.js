aff93d81cd6e316acca07c17ea8f217f1dc6b1bd
Ayush-Srivastava-northeastern <77870719+Ayush-Srivastava-northeastern@users.noreply.github.com>
2021-04-21 14:14:09 -0400

    documenting

--- webapp/src/Components/UserAuth/PrivateRoute.js
+++ webapp/src/Components/UserAuth/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./index";
-
+//private Route is used when we want to authenticate and it redirect to signIn page used by signIn page
 const PrivateRoute = ({ component: Component, ...rest }) => (
 
     <Route
